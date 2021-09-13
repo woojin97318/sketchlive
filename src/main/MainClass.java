1f7d5af85611e2b30032ba0a46bf0c66893bd4e0
woojin97318 <woojin97318@naver.com>
2021-09-13 14:38:56 +0900

    로그인 회원가입 fxml 대충완성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,13 +1,28 @@
 package main;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import signin.SignInController;
 
 public class MainClass extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader =
+				new FXMLLoader(getClass().getResource("../signin/signInView.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+
+		SignInController ctl = loader.getController();
+		ctl.setRoot(root);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
--- src/signin/SignInController.java
+++ src/signin/SignInController.java
@@ -1,5 +1,28 @@
 package signin;
 
-public class SignInController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class SignInController implements Initializable{
+	Parent root;
+	
+	public void setRoot(Parent root) {
+		this.root = root;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+	
+	public void loginBtn() {
+		
+	}
+	public void SignUpBtn() {
+		
+	}
 
 }
